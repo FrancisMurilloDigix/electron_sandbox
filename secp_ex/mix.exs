f6a7e31a25ff5414a628191331d397f20a59af07
Francis Murillo <francismurillo@digix.global>
2019-11-19 10:26:39 +0800

    Next step

--- secp_ex/mix.exs
+++ secp_ex/mix.exs
@@ -21,10 +21,23 @@ defmodule SecpEx.MixProject do
     [
       {:blockchain, "~> 0.1.7"},
       {:exth_crypto, "~> 0.1.4"},
-      {:libsecp256k1, "~> 0.1.9"},
+      # {:libsecp256k1, "~> 0.1.9"},
       # {:libsecp256k1, git: "https://github.com/DigixGlobal/libsecp256k1.git", branch: "fix/freebsd", override: true},
-      {:eleveldb, "2.2.20", override: true, manager: :rebar3},
-      # {:keccakf1600, "~> 3.0.0", override: true}
-    ]
+      {:eleveldb, "2.2.20", override: true, manager: :rebar3}
+      # {:keccakf1600, "~> 2.0.0", override: true}
+    ]  ++
+      bsd_deps()
+  end
+
+  defp bsd_deps do
+    case :os.type() do
+      {:unix, type} when type in [:freebsd, :openbsd] ->
+        [
+          {:eleveldb, "2.2.20", override: true, manager: :rebar}
+        ]
+
+      _ ->
+        []
+    end
   end
 end
