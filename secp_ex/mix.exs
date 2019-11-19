367d44632579b5fcba1844e65af0497cc634aae8
Francis Murillo <francismurillo@digix.global>
2019-11-19 09:50:06 +0800

    Add keccak override

--- secp_ex/mix.exs
+++ secp_ex/mix.exs
@@ -22,7 +22,8 @@ defmodule SecpEx.MixProject do
       {:blockchain, "~> 0.1.7"},
       {:exth_crypto, "~> 0.1.4"},
       {:libsecp256k1, git: "https://github.com/DigixGlobal/libsecp256k1.git", branch: "fix/freebsd", override: true},
-      {:eleveldb, "2.2.20", override: true, compile: "rebar compile"}
+      {:eleveldb, "2.2.20", override: true, compile: "rebar compile"},
+      {:keccakf1600, "~> 3.0.0", override: true}
     ]
   end
 end
