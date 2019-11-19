949183bb9b67e6116100d885bb556aeebbf725bf
Francis Murillo <francismurillo@digix.global>
2019-11-19 13:14:25 +0800

    Next step +4

--- secp_ex/mix.exs
+++ secp_ex/mix.exs
@@ -23,7 +23,6 @@ defmodule SecpEx.MixProject do
       {:exth_crypto, "~> 0.1.4"},
       # {:libsecp256k1, "~> 0.1.9"},
       # {:libsecp256k1, git: "https://github.com/DigixGlobal/libsecp256k1.git", branch: "fix/freebsd", override: true},
-      {:eleveldb, "2.2.20", manager: :rebar3}
       # {:keccakf1600, "~> 2.0.0", override: true}
     ]  ++
       bsd_deps()
@@ -38,7 +37,7 @@ defmodule SecpEx.MixProject do
         ]
 
       _ ->
-        []
+        [{:eleveldb, "2.2.20", manager: :rebar3}]
     end
   end
 end
